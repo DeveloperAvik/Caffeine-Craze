0d19df1fe79dab4f030c07e18e70c9cb35af55bb
Aditto <avikd906@proton.me>
2024-12-04 01:26:41 +0530

    update single coffee information

--- backend/index.js
+++ backend/index.js
@@ -32,10 +32,30 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/coffee:id", async (req, res) => {
+    
+    app.get("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+    
+
+
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: req.body
+      };
+
+      const result = await coffeeCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+
       res.send(result);
     });
 
